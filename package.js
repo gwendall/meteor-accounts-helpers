b0440ac6c88d4fc0651845410a0c8a15587831b9
Gwen <gwendall.esnault@gmail.com>
2015-10-17 14:55:25 +0200

    misc

--- package.js
+++ package.js
@@ -1,11 +1,12 @@
 Package.describe({
   name: 'gwendall:accounts-helpers',
   summary: 'Useful helpers for Accounts',
-  version: '0.1.3',
+  version: '0.1.4',
   git: 'https://github.com/gwendall/meteor-accounts-helpers'
 });
 
 var packages = [
+  'check@1.0.6',
   'accounts-base@1.2.1',
   'accounts-ui@1.1.6',
   'underscore@1.0.4',
